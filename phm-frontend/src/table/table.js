1f23ac5b08cdd9fe0b1aadd8479730363e1fedba
facusacchi <faq.ndo.n.s@gmail.com>
2021-03-15 00:39:41 -0300

    agrego botones de aceptar y cancelar en el componente table

--- phm-frontend/src/table/table.js
+++ phm-frontend/src/table/table.js
@@ -2,6 +2,8 @@ import React, {useState} from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import {Checkbox} from 'primereact/checkbox';
+import { Button } from 'primereact/button'
+import './table.css'
 
 
 const Table = () => {
@@ -9,11 +11,12 @@ const Table = () => {
                         {id: 3, descripcion: "respuesta 3", elegida: false}, {id: 4, descripcion: "respuesta 4", elegida: false},
                         {id: 5, descripcion: "respuesta 5", elegida: false}, {id: 6, descripcion: "respuesta 6", elegida: false}]
     
+    const pregunta = "¿ Cual es la respuesta ?"
     const [respuestas, setRespuestas] = useState(respuestasArray)
     
     const seleccionar = (respuesta) => {
         return(
-            <Checkbox onChange={() => setChckbx(respuesta.id)} checked={respuesta.elegida}></Checkbox>
+            <Checkbox className="chckbx" onChange={() => setChckbx(respuesta.id)} checked={respuesta.elegida}></Checkbox>
             )
         }
     
@@ -27,12 +30,26 @@ const Table = () => {
         setRespuestas(updatedList)
     }
 
+    const aceptar = () => {
+        
+    }
+
+    const cancelar = () => {
+        
+    }
+
     return(
         <div className="">
             <DataTable value={respuestas} autoLayout={true}>
-                <Column className ="" field="descripcion" header="RESPUESTAS"></Column>
+                <Column className ="" field="descripcion" header={pregunta}></Column>
                 <Column className ="" body={seleccionar}></Column>
             </DataTable>
+            <div className ="buttonsdiv">
+                <div className ="buttons">
+                <Button label="Aceptar" className="p-button-secondary" onClick={() => aceptar} />
+                <Button label="Cancelar" className="p-button-secondary" onClick={() => cancelar} />
+                </div>
+            </div>
         </div>
     )
 }
