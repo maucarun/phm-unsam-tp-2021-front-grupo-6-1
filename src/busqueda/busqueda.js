e1650ed97e5232d9e36d5d8ef2c989948b608727
facusacchi <faq.ndo.n.s@gmail.com>
2021-03-17 19:24:38 -0300

    modifico comp de busqueda

--- src/busqueda/busqueda.js
+++ src/busqueda/busqueda.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
+import {Checkbox} from 'primereact/checkbox';
 import './busqueda.css'
 
 const Busqueda = () => {
+    const [ckeckbx, setCheckbx] = useState(false)
+
+    
+
     return (
         <div className="busqueda">
             <span className="title">Busqueda de Preguntas</span>
             <div className="body">
                 <InputText  className="inputtext" />
                 <Button id="button" icon="pi pi-search iconoBusqueda" iconPos="right" />
+            </div>
+            <div className="chckbx-desc">
+                <Checkbox className="" onChange={() => setCheckbx(!ckeckbx)} checked={ckeckbx}></Checkbox>
+                <span className="chckbx-span">Solo activas</span>
             </div>
         </div>
     )
