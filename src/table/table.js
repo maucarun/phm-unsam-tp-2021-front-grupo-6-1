6493e50463fe50f8f5c5d7214fe2c02ed3cc39a5
facusacchi <faq.ndo.n.s@gmail.com>
2021-03-19 15:36:51 -0300

    traigo pregunta por id del backend para presentarla en la pantalla 4

--- src/table/table.js
+++ src/table/table.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import {Checkbox} from 'primereact/checkbox';
 import { Button } from 'primereact/button'
 import './table.css'
+import { preguntaService } from "../services/pregunta-service";
+import Pregunta from "../dominio/pregunta";
 
 
-const Table = () => {
+
+const Table = ({history, match}) => {
+
+    useEffect(()=>{
+        buscarPregunta()
+    }, [])
+
     const respuestasArray = [{id: 1, descripcion: "respuesta 1", elegida: false}, {id: 2, descripcion: "respuesta 2", elegida: false}, 
                         {id: 3, descripcion: "respuesta 3", elegida: false}, {id: 4, descripcion: "respuesta 4", elegida: false},
                         {id: 5, descripcion: "respuesta 5", elegida: false}, {id: 6, descripcion: "respuesta 6", elegida: false}]
-    
-    const pregunta = "¿ Cual es la respuesta ?"
+                        
+    const [pregunta, setPregunta] = useState(new Pregunta())
     const [respuestas, setRespuestas] = useState(respuestasArray)
+                      
+    const buscarPregunta = async() => {
+        const preg = await preguntaService.getPregunta(match.params.id)
+        setPregunta(preg)
+    }
     
     const seleccionar = (respuesta) => {
         return(
@@ -35,25 +48,25 @@ const Table = () => {
         return respuestas.map(respuesta => respuesta.elegida).includes(true)
     }
 
-    const aceptar = () => {
-        
+    const aceptar = async() => {
+
     }
 
     const cancelar = () => {
-        
+        history.push("/busqueda")
     }
 
     return(
         <div className="container-table">
             <span className="autor">Autor: Pepe Palala</span>
             <DataTable value={respuestas} autoLayout={true}>
-                <Column className ="descripcion" field="descripcion" header={pregunta}></Column>
+                <Column className ="descripcion" field="descripcion" header={pregunta.descripcion}></Column>
                 <Column className ="" body={seleccionar}></Column>
             </DataTable>
             <div className ="buttonsdiv">
                 <div className ="buttons">
-                <Button label="Aceptar" className="p-button-primary" onClick={() => aceptar} />
-                <Button label="Cancelar" className="p-button-secondary" onClick={() => cancelar} />
+                <Button label="Aceptar" className="p-button-primary" onClick={() => aceptar()} />
+                <Button label="Cancelar" className="p-button-secondary" onClick={() => cancelar()} />
                 </div>
             </div>
         </div>
