127afd5e5ca796cc7fe3d8babde8414a9da50d88
NannFernandez <nanfernandezok@gmail.com>
2021-03-20 08:25:39 -0300

    perfil usuario se agrega modal a agregar un amigo, se agrega componente tabla

--- src/perfil_usuario/perfil.js
+++ src/perfil_usuario/perfil.js
@@ -3,69 +3,116 @@ import "./perfil.css";
 import { InputText } from "primereact/inputtext";
 import Usuario from "../dominio/usuario";
 import { usuarioService } from "../services/usuario-service";
-import { Calendar } from 'primereact/calendar';
+import { Calendar } from "primereact/calendar";
+import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Dialog } from "primereact/dialog";
 
 class Perfil extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      usuario:[],
+      usuario: new Usuario(),
+      mostrarModal: false,
     };
   }
- 
+
   async componentDidMount() {
     try {
-      const usuario = await usuarioService.getUsuario(usuarioService.userLogged.id)
-      console.dir(usuario)
-      this.cambiarEstado(usuario)
-    }catch(e) {
+      const usuario = await usuarioService.getUsuario(
+        usuarioService.userLogged.id
+      );
+      this.cambiarEstado(usuario);
+    } catch (e) {
       //this.addMessages(e)
     }
   }
 
-  setNombre(nombre_usuario){
-    const usuario = this.state.usuario
-    usuario.nombre = nombre_usuario
-    this.cambiarEstado(usuario)
+  setNombre(nombre_usuario) {
+    const usuario = this.state.usuario;
+    usuario.nombre = nombre_usuario;
+    this.cambiarEstado(usuario);
   }
 
   cambiarEstado(usuario) {
     this.setState({
-      usuario:usuario,
-    })
+      usuario: usuario,
+    });
   }
 
   setApellido(apellido_usuario) {
-    const usuario = this.state.usuario
-    usuario.apellido = apellido_usuario
-    this.cambiarEstado(usuario)
+    const usuario = this.state.usuario;
+    usuario.apellido = apellido_usuario;
+    this.cambiarEstado(usuario);
   }
 
+  setFechaDeNacimiento(fecha_nacimiento) {
+    const usuario = this.state.usuario;
+    usuario.fechaDeNacimiento = fecha_nacimiento;
+    this.cambiarEstado(usuario);
+  }
 
+  agregar() {
+    this.setState({ mostrarModal: true });
+  }
+
+  cerrar() {
+    this.setState({ mostrarModal: false });
+  }
 
   render() {
     return (
       <div className="card">
         <section className="encabezado">
-          <h1>{this.state.usuario.nombre} {this.state.usuario.apellido}</h1>
+          <h1>
+            {this.state.usuario.nombre} {this.state.usuario.apellido}
+          </h1>
           <h1>{this.state.usuario.puntaje} Puntos</h1>
         </section>
+
         <section className="inputs1">
-        <h5>Nombre</h5>
-        <InputText value={this.state.usuario.nombre}
-          onChange={(event) => this.setNombre(event.target.value)}
-        />
-        <h5>Apellido</h5>
-        <InputText value={this.state.usuario.apellido}
-          onChange={(event) => this.setApellido(event.target.value)}
+          <h5>Nombre</h5>
+          <InputText
+            value={this.state.usuario.nombre}
+            onChange={(event) => this.setNombre(event.target.value)}
           />
-          </section>
-          <section>
-            <h5>Nacimiento</h5>
-            <Calendar value={this.state.usuario.fechaDeNacimiento} onChange={(e) => setDate(e.value)} showIcon></Calendar>
-          </section>
-        
-        
+          <h5>Apellido</h5>
+          <InputText
+            value={this.state.usuario.apellido}
+            onChange={(event) => this.setApellido(event.target.value)}
+          />
+        </section>
+        <section className="inputs1">
+          <h5>Nacimiento</h5>
+          <Calendar
+            value={this.state.usuario.fechaDeNacimiento}
+            onChange={(event) => this.setFechaDeNacimiento(event.value)}
+            showIcon
+          ></Calendar>
+          <h5>Amigos</h5>
+          <Button
+            label="Agregar"
+            className="p-button-primary"
+            onClick={() => this.agregar()}
+          />
+        </section>
+
+        <Dialog
+          header="Header"
+          visible={this.state.mostrarModal}
+          style={{ width: "50vw" }}
+          // footer={renderFooter("displayBasic")}
+          onHide={() => this.cerrar()}
+          baseZIndex={1000}
+        ></Dialog>
+
+        <section className="tabla">
+          <DataTable value={this.state.usuario}>
+            <Column field="code" header="Code"></Column>
+          </DataTable>
+        </section>
+
         <section></section>
       </div>
     );
