d069814c568af055b9d05551f1a73af33f6c0271
maucarun <42684372+maucarun@users.noreply.github.com>
2021-04-02 01:17:19 -0300

    Se actualizan puntos de userLogged al responder pregunta

--- src/table/table.js
+++ src/table/table.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect, createRef} from "react";
+import React, { useState, useEffect, createRef } from "react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import {Checkbox} from 'primereact/checkbox';
+import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button'
 import './table.css'
 import { preguntaService } from "../services/pregunta-service";
@@ -11,12 +11,12 @@ import { confirmPopup } from 'primereact/confirmpopup'
 import { Dialog } from 'primereact/dialog';
 import { Toast } from 'primereact/toast';
 
-const Table = ({history, match}) => {
+const Table = ({ history, match }) => {
 
     useEffect(async () => {
         await buscarPregunta()
     }, [])
-    
+
     const toast = createRef()
     const [pregunta, setPregunta] = useState(new Pregunta())
     const [opciones, setOpciones] = useState([])
@@ -26,7 +26,7 @@ const Table = ({history, match}) => {
     const [displaySuccess, setDisplaySuccess] = useState(false)
     const [displayIncorrect, setDisplayIncorrect] = useState(false)
     const [displayInactive, setDisplayInactive] = useState(false)
-    
+
     const buscarPregunta = async () => {
         try {
             const preg = await preguntaService.getPregunta(match.params.id)
@@ -34,35 +34,35 @@ const Table = ({history, match}) => {
             setNombreAutor(preg.autor.nombre)
             setApellidoAutor(preg.autor.apellido)
             convertirOpciones(preg.opciones)
-        } catch(error) {
-            toast.current.show({ severity: 'error', summary: 'Ocurrió un error al buscar la pregunta', detail: error.message, life: 3000})
+        } catch (error) {
+            toast.current.show({ severity: 'error', summary: 'Ocurrió un error al buscar la pregunta', detail: error.message, life: 3000 })
         }
     }
-    
+
     const convertirOpciones = (options) => {
         const opcionesConvertidas = []
         options.forEach(op => {
-            opcionesConvertidas.push({descripcion: op, elegida: false})
+            opcionesConvertidas.push({ descripcion: op, elegida: false })
         })
         setOpciones(opcionesConvertidas)
     }
-    
+
     const seleccionar = (opcion) => {
-        return(
+        return (
             <Checkbox className="chckbx" onChange={() => setChckbx(opcion)} checked={opcion.elegida}></Checkbox>
-            )
-        }
-    
-        const setChckbx = (opcion) => {
-            const updatedOptions = opciones.map(op => {
-                if(opcion.descripcion === op.descripcion) {
+        )
+    }
+
+    const setChckbx = (opcion) => {
+        const updatedOptions = opciones.map(op => {
+            if (opcion.descripcion === op.descripcion) {
                 op.elegida = !op.elegida
             }
             return op
         })
         setOpciones(updatedOptions)
     }
-    
+
     const confirm = (opcion) => {
         confirmPopup({
             message: "¿ Confirmar Respuesta ?",
@@ -71,41 +71,41 @@ const Table = ({history, match}) => {
             reject: () => rejectFunc()
         });
     }
-    
+
     const rejectFunc = () => {
 
     }
-    
+
     const acceptFunc = async (opcionElegida) => {
         await actualizarUser(opcionElegida)
         try {
-           const _pregunta = await preguntaService.getPregunta(match.params.id)
-           if(opcionElegida !== _pregunta.respuestaCorrecta) {
-               setDisplayIncorrect(true)
-           } else if(!_pregunta.activa) {
-               setDisplayInactive(true)
-           } else {
-               setDisplaySuccess(true)
-           }
-        } catch(error) {
+            const _pregunta = await preguntaService.getPregunta(match.params.id)
+            if (opcionElegida !== _pregunta.respuestaCorrecta) {
+                setDisplayIncorrect(true)
+            } else if (!_pregunta.activa) {
+                setDisplayInactive(true)
+            } else {
+                setDisplaySuccess(true)
+            }
+        } catch (error) {
             console.log("No se trajo la pregunta")
             console.log(_pregunta)
             console.log(_pregunta.respuestaCorrecta)
         }
     }
-    
+
     const actualizarUser = async (opcionElegida) => {
-        const opcionJson = { "opcionElegida": opcionElegida,  "pregunta": pregunta.descripcion}
+        const opcionJson = { "opcionElegida": opcionElegida, "pregunta": pregunta.descripcion }
         try {
             await usuarioService.actualizarUsuario(usuarioService.userLogged.id, pregunta.id, opcionJson)
-            //usuarioService.userLogged = await usuarioService.getUsuario(usuarioService.userLogged.id)
-        } catch(error) {
-            toast.current.show({ severity: 'error', summary: 'Ocurrió un error al actualizar sus datos', detail: error.message, life: 3000})
+            usuarioService.userLogged = await usuarioService.getUsuario(usuarioService.userLogged.id)
+        } catch (error) {
+            toast.current.show({ severity: 'error', summary: 'Ocurrió un error al actualizar sus datos', detail: error.message, life: 3000 })
         }
     }
-    
+
     const aceptar = async () => {
-        if(soloUnaOpcionSeleccionada()) {
+        if (soloUnaOpcionSeleccionada()) {
             setMensajeDeError(false)
             const opSelec = opcionSeleccionada()
             confirm(opSelec)
@@ -113,30 +113,30 @@ const Table = ({history, match}) => {
             setMensajeDeError(true)
         }
     }
-    
+
     const cancelar = () => {
         history.push("/busqueda")
     }
-    
+
     const soloUnaOpcionSeleccionada = () => {
         return filtrarOpcionesSeleccionadas().length === 1
     }
-    
+
     const filtrarOpcionesSeleccionadas = () => {
         const opcionesSeleccionadas = opciones.filter(op => op.elegida === true)
         return opcionesSeleccionadas
     }
-    
+
     const opcionSeleccionada = () => {
-        const op = filtrarOpcionesSeleccionadas()    
+        const op = filtrarOpcionesSeleccionadas()
         return op[0].descripcion
     }
-    
+
     const cerrarPantallaSuccess = () => {
         setDisplaySuccess(false)
         history.push("/busqueda")
     }
-    
+
     const cerrarPantallaIncorrect = () => {
         setDisplayIncorrect(false)
         history.push("/busqueda")
@@ -145,40 +145,40 @@ const Table = ({history, match}) => {
     const cerrarPantallaInactiva = () => {
         setDisplayInactive(false)
         history.push("/busqueda")
-    } 
-    
-    return(
+    }
+
+    return (
         <div className="container-table">
             <div className="table-and-span">
                 <span className="autor">Autor: {nombreAutor} {apellidoAutor}</span>
                 <DataTable value={opciones} autoLayout={true}>
-                    <Column className ="descripcion" field="descripcion" header={pregunta.descripcion}></Column>
-                    <Column className ="" body={seleccionar}></Column>
+                    <Column className="descripcion" field="descripcion" header={pregunta.descripcion}></Column>
+                    <Column className="" body={seleccionar}></Column>
                 </DataTable>
             </div>
-            <div className ="buttonsdiv">
+            <div className="buttonsdiv">
                 <div>
                     {mensajeDeError && <span className="validacion-opciones">Debe seleccionar solo una opcion</span>}
                 </div>
-                <div className ="buttons">
-                <Button label="Aceptar" className="p-button-rounded p-button-success" onClick={() => aceptar()} />
-                <Button label="Cancelar" className="p-button-rounded p-button-danger" onClick={() => cancelar()} />
+                <div className="buttons">
+                    <Button label="Aceptar" className="p-button-rounded p-button-success" onClick={() => aceptar()} />
+                    <Button label="Cancelar" className="p-button-rounded p-button-danger" onClick={() => cancelar()} />
                 </div>
             </div>
             <Dialog header="¡ Felicitaciones !" visible={displaySuccess} style={{ width: '50vw' }} onHide={() => cerrarPantallaSuccess()}>
-                <span className="congratulations">Usted ha respondido correctamente</span><br/>
-                <span className="congratulations">Puntos sumados: {pregunta.puntos}</span><br/>
+                <span className="congratulations">Usted ha respondido correctamente</span><br />
+                <span className="congratulations">Puntos sumados: {pregunta.puntos}</span><br />
                 <span className="congratulations">Puntos totales: {usuarioService.userLogged.puntaje}</span>
             </Dialog>
             <Dialog header="Incorrecto" visible={displayInactive} style={{ width: '50vw' }} onHide={() => cerrarPantallaInactiva()}>
-                <span className="no-active">Su respuesta es correcta</span><br/>
-                <span className="no-active">pero la pregunta respondida esta inactiva</span><br/>
-                <span className="no-active">No ha sumado puntos</span><br/>
+                <span className="no-active">Su respuesta es correcta</span><br />
+                <span className="no-active">pero la pregunta respondida esta inactiva</span><br />
+                <span className="no-active">No ha sumado puntos</span><br />
                 <span className="no-active">Puntos totales: {usuarioService.userLogged.puntaje}</span>
             </Dialog>
             <Dialog header="Incorrecto" visible={displayIncorrect} style={{ width: '50vw' }} onHide={() => cerrarPantallaIncorrect()}>
-                <span className="no-congratulations">Su respuesta es incorrecta</span><br/>
-                <span className="no-congratulations">No ha sumado puntos</span><br/>
+                <span className="no-congratulations">Su respuesta es incorrecta</span><br />
+                <span className="no-congratulations">No ha sumado puntos</span><br />
                 <span className="no-congratulations">Puntos totales: {usuarioService.userLogged.puntaje}</span>
             </Dialog>
             <Toast ref={toast} />
