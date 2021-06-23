3df9b2086b3795597f91b8baeebc027b23e343ea
maucarun <42684372+maucarun@users.noreply.github.com>
2021-06-23 01:43:50 -0300

    Avanzamos con entrega 4

--- README.md
+++ README.md
@@ -1,70 +1,25 @@
-# Getting Started with Create React App
+semilla:ritual tip pudding smoke access tide peasant iron twelve dignity tornado run
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+``` java
+truffle migrate
+truffle compile
+truffle console
 
-## Available Scripts
+var preguntasInstance = await Pregunta3.new()
 
-In the project directory, you can run:
+preguntasInstance.getRespuestasByAddress('0x44026F6C64C2c98Ae7D4FbC15B20ae730B52AC8C')
 
-### `npm start`
+//prueba de require "No se puede responder una pregunta propia":
+preguntasInstance.responder('0x44026F6C64C2c98Ae7D4FbC15B20ae730B52AC8C',{"idPregunta": 1,"opcion": "opcionDePrueba","puntaje":15})
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+preguntasInstance.responder('0x695825d99f71256bfb7ae199F894C15Ca399dD4E',{"idPregunta": 1,"opcion": "opcionDePrueba","puntaje":15})
+preguntasInstance.responder({"idPregunta": 1,"opcion": "opcionDePrueba","puntaje":15})
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+preguntasInstance.getRespuestasByAddress('0x695825d99f71256bfb7ae199F894C15Ca399dD4E')
 
-### `npm test`
+preguntasInstance.getPreguntaById(2)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+preguntasInstance.cambiarEstado(3)
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+preguntasInstance.crearPregunta({"idPregunta": 1, "autor": "0x44026F6C64C2c98Ae7D4FbC15B20ae730B52AC8C", "texto": "prueba", "opciones": ["prueba1","prueba2"], "opcionCorrecta": "prueba2"})
+```
--- pregunta3/truffle/contracts/Pregunta3.sol
+++ pregunta3/truffle/contracts/Pregunta3.sol
@@ -3,82 +3,13 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Pregunta3 {
-
-    Estado estado = Estado.ACTIVO;
-    //function promedioPuntaje -> address
-    //function cambiarEstado -> address, estadoNuevo
-    //require cambiarEstado -> Solo el creador puede cambiar el estado del smart contract
-
-    struct Pregunta {
-        uint256 idPregunta; // AUTOINCREMENTAL -> variable global? modifier?
-        address autor;
-        string texto;
-        string[] opciones;
-        string opcionCorrecta;
-    }
-
-    struct Respuesta {
-        address usuario;
-        uint256 idPregunta;
-        string opcion;
-        uint256 puntaje;
-    }
-
-    struct Usuario {
-        address id;
-        Respuesta[] respuestas;
-    }
+    address duenio = 0x44026F6C64C2c98Ae7D4FbC15B20ae730B52AC8C;
+    uint256 idIncremental = 0;
+    Estado public estado = Estado.ACTIVO;
 
     Pregunta[] public preguntas;
-    Respuesta[] public respuestas;
-    Usuario [] public usuarios;
 
-    uint256 idIncremental = 0;
-
-    function getPreguntaById(uint256 idPregunta)
-        public
-        view
-        returns (Pregunta memory pregunta)
-    {
-        for (uint256 i = 0; i < preguntas.length; i++) {
-            if (preguntas[i].idPregunta == idPregunta) {
-                pregunta = preguntas[i];
-                //return pregunta;
-            }
-        }
-    }
-
-    //function responder -> address (del que responde), respuesta: idPregunta, opcion, puntaje
-    function responder(Respuesta memory respuesta) public {
-        Pregunta memory pregunta = getPreguntaById(respuesta.idPregunta);
-        require(
-            respuesta.usuario != pregunta.autor,
-            "No se puede responder una pregunta propia"
-        );
-        respuestas.push(respuesta);
-    }
-
-    //function crearPregunta -> pregunta: address (autor), texto, opciones (array?), opcionCorrecta
-    function crearPregunta(Pregunta memory pregunta) public {
-        pregunta.idPregunta = getId();
-        preguntas.push(pregunta);
-    }
-
-    function getId() private returns(uint) { return ++idIncremental; }
-
-    // Un usuario debe poder obtener el promedio aproximado de los puntajes de todas las respuestas que él hizo. 
-    // Esto debe al menos contar con precisión entera (no es necesario calcular con decimales).
-    function promedio(Usuario memory usuario) public pure returns (uint) {
-        Respuesta[] memory respuestasUsuario = usuario.respuestas;
-        uint puntos = 0;
-
-        for (uint256 i = 0; i < respuestasUsuario.length; i++) {
-            Respuesta memory respuesta = respuestasUsuario[i];
-            puntos += respuesta.puntaje;
-        }
-
-        return puntos / respuestasUsuario.length;
-    }
+    mapping(address => Respuesta[]) public usuario;
 
     enum Estado {
         ACTIVO,
@@ -86,6 +17,117 @@ contract Pregunta3 {
         RESPONDER,
         BOOTSTRAP
     }
+
+    struct Pregunta {
+        uint256 idPregunta;
+        address autor;
+        string texto;
+        string[] opciones;
+        string opcionCorrecta;
+    }
+
+    struct Respuesta {
+        uint256 idPregunta;
+        string opcion;
+        uint256 puntaje;
+    }
+    
+    /* 
+    ACTIVO toda la funcionalidad habilitada.
+    LECTURA sólo se pueden consultar preguntas existentes.
+    RESPONDER sólo se permite consultar y responder preguntas, pero no crear nuevas.
+    BOOTSTRAP sólo se permite crear nuevas preguntas. 
+     
+    modifier verificarEstado(Estado memory _estado) {
+        require(estado == _estado);
+        _;
+    } 
+    modifier alResponder() {
+        require(estado != Estado.BOOTSTRAP && estado != Estado.LECTURA);
+        _;
+    }
+
+    modifier consultarPreguntas() {
+        require(estado != Estado.BOOTSTRAP);
+        _;
+    }
+
+    modifier consultarRespuestas() {
+        require(estado != Estado.BOOTSTRAP && estado != Estado.RESPONDER);
+        _;
+    }
+
+    modifier generarPregunta() {
+        require(estado != Estado.LECTURA && estado != Estado.RESPONDER);
+        _;
+    } 
+    */
+
+    function verificarEstado(Estado _estado) public view {
+        require(estado != _estado, "Estado invalido");
+    }
+
+    //function cambiarEstado -> address, estadoNuevo
+    //require cambiarEstado -> Solo el creador puede cambiar el estado del smart contract
+    function cambiarEstado(Estado _estado) public { 
+        require(msg.sender == duenio);
+        estado = _estado;
+    }
+
+    function getPreguntaById(uint256 idPregunta) public view returns (Pregunta memory pregunta) {
+        verificarEstado(Estado.BOOTSTRAP);
+        for (uint256 i = 0; i < preguntas.length; i++) {
+            if (preguntas[i].idPregunta == idPregunta) {
+                pregunta = preguntas[i];
+            }
+        }
+    }
+
+    //function responder -> address (del que responde), respuesta: idPregunta, opcion, puntaje
+    function responder(Respuesta memory respuesta) public {
+        verificarEstado(Estado.BOOTSTRAP);
+        verificarEstado(Estado.LECTURA);
+        Pregunta memory pregunta = getPreguntaById(respuesta.idPregunta);
+        require(
+            msg.sender != pregunta.autor,
+            "No se puede responder una pregunta propia"
+        );
+        usuario[msg.sender].push(respuesta);
+    }
+
+    //function crearPregunta -> pregunta: address (autor), texto, opciones (array?), opcionCorrecta
+    function crearPregunta(Pregunta memory pregunta) public {
+        verificarEstado(Estado.LECTURA);
+        verificarEstado(Estado.RESPONDER);
+        pregunta.idPregunta = getId();
+        preguntas.push(pregunta);
+    }
+
+    function getId() private returns (uint256) {
+        return ++idIncremental;
+    }
+
+    function getRespuestasByAddress(address idUsuario) private view returns (Respuesta[] memory respuestas){
+        respuestas = usuario[idUsuario];
+    }
+
+    //function promedioPuntaje -> address
+    // Un usuario debe poder obtener el promedio aproximado de los puntajes de todas las respuestas que él hizo.
+    // Esto debe al menos contar con precisión entera (no es necesario calcular con decimales).
+    /* 
+    function promedio(Usuario memory usuario) public pure returns (uint256) {
+        Respuesta[] memory respuestasUsuario = usuario.respuestas;
+        uint256 puntos = 0;
+
+        for (uint256 i = 0; i < respuestasUsuario.length; i++) {
+            Respuesta memory respuesta = respuestasUsuario[i];
+            puntos += respuesta.puntaje;
+        }
+
+        return puntos / respuestasUsuario.length;
+    } 
+    */
+
 
     /* 
     function crearPregunta(address _autor, string _texto, string[] memory _opciones, string _opcionCorrecta) public {
@@ -96,15 +138,9 @@ contract Pregunta3 {
             opcionCorrecta: _opcionCorrecta
         }));
     } 
+    
+    function getPreguntas() public view returns (Pregunta[] memory listaPreguntas) {
+        listaPreguntas = preguntas;
+    }
     */
-    // function getPreguntas()
-    //     public
-    //     view
-    //     returns (Pregunta[] memory listaPreguntas)
-    // {
-    //     listaPreguntas = preguntas;
-    // }
 }
-
-//semilla:ritual tip pudding smoke access tide peasant iron twelve dignity tornado run
-//preguntasInstance.crearPregunta({"idPregunta": 1, "autor": "0x44026F6C64C2c98Ae7D4FbC15B20ae730B52AC8C", "texto": "prueba", "opciones": ["prueba1","prueba2"], "opcionCorrecta": "prueba2"})
